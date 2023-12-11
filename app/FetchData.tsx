546c44b6b26276ba46426148be457c59ba2445b6
konami2 <abdelmottaliblol@gmail.com>
2023-12-11 11:05:45 +0100

    localstorage checks for ssr

--- app/FetchData.tsx
+++ app/FetchData.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState, useEffect } from "react";
 import {
   createAndShuffleDeck,
@@ -47,14 +48,30 @@ const HomePage = () => {
   const [clickable, setClickable] = useState(false);
   const [clickableButtons, setClickableButtons] = useState(true);
   const [loading, setLoading] = useState(true);
+  const isLocalStorageAvailable = () => {
+    try {
+      const testKey = "__test__";
+      localStorage.setItem(testKey, testKey);
+      localStorage.removeItem(testKey);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
   const [shortestTimeEasy, setShortestTimeEasy] = useState<number>(
-    parseInt(localStorage.getItem("shortestTimeEasy") || "0", 10),
+    isLocalStorageAvailable()
+      ? parseInt(localStorage.getItem("shortestTimeEasy") || "0", 10)
+      : 0
   );
   const [shortestTimeMedium, setShortestTimeMedium] = useState<number>(
-    parseInt(localStorage.getItem("shortestTimeMedium") || "0", 10),
+    isLocalStorageAvailable()
+      ? parseInt(localStorage.getItem("shortestTimeMedium") || "0", 10)
+      : 0
   );
   const [shortestTimeHard, setShortestTimeHard] = useState<number>(
-    parseInt(localStorage.getItem("shortestTimeHard") || "0", 10),
+    isLocalStorageAvailable()
+      ? parseInt(localStorage.getItem("shortestTimeHard") || "0", 10)
+      : 0
   );
 
   useEffect(() => {
@@ -77,14 +94,14 @@ const HomePage = () => {
           const count = difficulty.easy
             ? 10
             : difficulty.medium
-              ? 16
-              : difficulty.hard
-                ? 20
-                : 10;
+            ? 16
+            : difficulty.hard
+            ? 20
+            : 10;
           const drawnCards = await drawCards(deckId, count);
           //the glitch can be fixed with this need to have the component in position
           //absolute and use the use the timeout to avoid the glitch
-          setTimeout(() => { });
+          setTimeout(() => {});
           setLoading(false);
           const duplicatedCards = [...drawnCards, ...drawnCards];
           const shuffledCards = duplicatedCards.sort(() => Math.random() - 0.5);
@@ -93,7 +110,7 @@ const HomePage = () => {
               ...card,
               hidden: true,
               index: index,
-            }),
+            })
           );
           modifiedCards.forEach((card: Card, index: number) => {
             setTimeout(() => {
@@ -101,8 +118,8 @@ const HomePage = () => {
                 prevCards.map((prevCard, prevIndex) =>
                   prevIndex === index
                     ? { ...prevCard, hidden: false }
-                    : prevCard,
-                ),
+                    : prevCard
+                )
               );
               setClickable(false);
             }, index * 100);
@@ -116,10 +133,10 @@ const HomePage = () => {
                     prevCards.map((prevCard, prevIndex) =>
                       prevIndex === index
                         ? { ...prevCard, hidden: true }
-                        : prevCard,
-                    ),
+                        : prevCard
+                    )
                   );
-                }, index * 100),
+                }, index * 100)
               );
 
               Promise.all(promises).then(() => {
@@ -128,7 +145,7 @@ const HomePage = () => {
                 setInitialReveal(false);
               });
             },
-            difficulty.hard ? 8000 : 4000,
+            difficulty.hard ? 8000 : 4000
           );
         } catch (error) {
           console.error("Error fetching data:", error);
@@ -142,28 +159,30 @@ const HomePage = () => {
   }, [playButtonClicked]);
 
   useEffect(() => {
-    if (cards.every((card) => !card.hidden) && !initialReveal) {
-      setShowConfetti(true);
-      setGameCompleted(true);
-      const currentShortestTime = getCurrentShortestTime(
-        difficulty,
-        shortestTimeEasy,
-        shortestTimeMedium,
-        shortestTimeHard,
-      );
-
-      if (time < currentShortestTime! || currentShortestTime === 0) {
-        setCurrentShortestTime(
-          time,
+    if (isLocalStorageAvailable()) {
+      if (cards.every((card) => !card.hidden) && !initialReveal) {
+        setShowConfetti(true);
+        setGameCompleted(true);
+        const currentShortestTime = getCurrentShortestTime(
           difficulty,
-          setShortestTimeEasy,
-          setShortestTimeMedium,
-          setShortestTimeHard,
+          shortestTimeEasy,
+          shortestTimeMedium,
+          shortestTimeHard
         );
-        setLocalStorageCurrentShortestTime(time, difficulty);
+
+        if (time < currentShortestTime! || currentShortestTime === 0) {
+          setCurrentShortestTime(
+            time,
+            difficulty,
+            setShortestTimeEasy,
+            setShortestTimeMedium,
+            setShortestTimeHard
+          );
+          setLocalStorageCurrentShortestTime(time, difficulty);
+        }
+      } else {
+        setShowConfetti(false);
       }
-    } else {
-      setShowConfetti(false);
     }
   }, [cards, initialReveal]);
 
@@ -173,7 +192,7 @@ const HomePage = () => {
     }
     if (
       flippedCards.every(
-        (card) => card.index !== clickedCard.index && clickedCard.hidden,
+        (card) => card.index !== clickedCard.index && clickedCard.hidden
       )
     ) {
       setMoves((prev) => prev + 1);
@@ -187,7 +206,7 @@ const HomePage = () => {
     }
 
     const updatedCards = cards.map((card) =>
-      card.index === clickedCard.index ? { ...card, hidden: false } : card,
+      card.index === clickedCard.index ? { ...card, hidden: false } : card
     );
     setCards(updatedCards);
 
@@ -209,9 +228,9 @@ const HomePage = () => {
           setMatchedCardsAnimation(false);
           const updatedMatchedCards = updatedCards.map((card) =>
             card.index === updatedFlippedCards[0].index ||
-              card.index === updatedFlippedCards[1].index
+            card.index === updatedFlippedCards[1].index
               ? { ...card, matched: true }
-              : card,
+              : card
           );
           setCards(updatedMatchedCards);
         }, 1000);
@@ -219,7 +238,7 @@ const HomePage = () => {
         setTimeout(() => {
           setFlippedCards([]);
           const modifiedCards = updatedCards.map((card) =>
-            !card.matched ? { ...card, hidden: true } : card,
+            !card.matched ? { ...card, hidden: true } : card
           );
           setCards(modifiedCards);
         }, 1500);
@@ -228,12 +247,13 @@ const HomePage = () => {
   };
   return (
     <div
-      className={`h-screen mx-auto flex flex-col items-center justify-center ${difficulty.easy
+      className={`h-screen mx-auto flex flex-col items-center justify-center ${
+        difficulty.easy
           ? "w-[550px]"
           : // : difficulty.medium
-          // ? "w-[950px]"
-          "w-[1000px]"
-        } text-white`}
+            // ? "w-[950px]"
+            "w-[1000px]"
+      } text-white`}
     >
       <GameHeader
         shortestTimeEasy={shortestTimeEasy}
