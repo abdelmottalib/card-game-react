bc6fc97010e7bba85e495aa499a20f22915216ed
konami2 <abdelmottaliblol@gmail.com>
2023-12-11 10:42:10 +0100

    local storage checks

--- app/utils.ts
+++ app/utils.ts
@@ -1,11 +1,11 @@
-import axios from 'axios';
-import { Dispatch, SetStateAction } from 'react';
-const DECK_API_BASE_URL = 'https://deckofcardsapi.com/api/deck';
+import axios from "axios";
+import { Dispatch, SetStateAction } from "react";
+const DECK_API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 
 // Create a new deck and shuffle it
 export async function createAndShuffleDeck() {
   const createDeckResponse = await axios.get(
-    `${DECK_API_BASE_URL}/new/shuffle/?deck_count=1`
+    `${DECK_API_BASE_URL}/new/shuffle/?deck_count=1`,
   );
   const deckId = createDeckResponse.data.deck_id;
 
@@ -15,7 +15,7 @@ export async function createAndShuffleDeck() {
 // Draw a specified number of cards from the deck
 export async function drawCards(deckId: string, count: number) {
   const drawCardResponse = await axios.get(
-    `${DECK_API_BASE_URL}/${deckId}/draw/?count=${count}`
+    `${DECK_API_BASE_URL}/${deckId}/draw/?count=${count}`,
   );
   const drawnCards = drawCardResponse.data.cards;
 
@@ -30,7 +30,7 @@ export const getCurrentShortestTime = (
   difficulty: Difficulty,
   shortestTimeEasy: number,
   shortestTimeMedium: number,
-  shortestTimeHard: number
+  shortestTimeHard: number,
 ) => {
   if (difficulty.easy) return shortestTimeEasy;
   else if (difficulty.medium) return shortestTimeMedium;
@@ -42,7 +42,7 @@ export const setCurrentShortestTime = (
   difficulty: Difficulty,
   setShortestTimeEasy: Dispatch<SetStateAction<number>>,
   setShortestTimeMedium: Dispatch<SetStateAction<number>>,
-  setShortestTimeHard: Dispatch<SetStateAction<number>>
+  setShortestTimeHard: Dispatch<SetStateAction<number>>,
 ) => {
   if (difficulty.easy) return setShortestTimeEasy(time);
   else if (difficulty.medium) return setShortestTimeMedium(time);
@@ -51,23 +51,28 @@ export const setCurrentShortestTime = (
 
 export const setLocalStorageCurrentShortestTime = (
   time: number,
-  difficulty: Difficulty
+  difficulty: Difficulty,
 ) => {
-  if (difficulty.easy)
-    localStorage.setItem('shortestTimeEasy', time.toString());
-  else if (difficulty.medium)
-    localStorage.setItem('shortestTimeMedium', time.toString());
-  else if (difficulty.hard)
-    localStorage.setItem('shortestTimeHard', time.toString());
+  if (typeof window !== "undefined" && window.localStorage) {
+    if (difficulty.easy)
+      localStorage.setItem("shortestTimeEasy", time.toString());
+    else if (difficulty.medium)
+      localStorage.setItem("shortestTimeMedium", time.toString());
+    else if (difficulty.hard)
+      localStorage.setItem("shortestTimeHard", time.toString());
+  }
 };
 export const formatTime = (seconds: number) => {
   const hours = Math.floor(seconds / 3600);
   const minutes = Math.floor((seconds % 3600) / 60);
   const remainingSeconds = seconds % 60;
 
-  return `${hours > 0 ? `${hours}h ` : ''}${minutes}m ${remainingSeconds}s`;
+  return `${hours > 0 ? `${hours}h ` : ""}${minutes}m ${remainingSeconds}s`;
 };
-export function delayAction(action: () => void, milliseconds: number): Promise<void> {
+export function delayAction(
+  action: () => void,
+  milliseconds: number,
+): Promise<void> {
   return new Promise((resolve) => {
     setTimeout(() => {
       action();
