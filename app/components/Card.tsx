42613f1d53edf172b7d8abce9e1cf1edb6a80bc3
konami2 <abdelmottaliblol@gmail.com>
2023-12-11 13:32:52 +0100

    deleted some comment s

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,4 +1,3 @@
-// CardComponent.js
 import React from 'react';
 import { motion } from 'framer-motion';
 
--- app/components/CardGrid.tsx
+++ app/components/CardGrid.tsx
@@ -1,4 +1,3 @@
-// CardGrid.js
 import React from 'react';
 import { motion } from 'framer-motion';
 import CardComponent from './Card';
